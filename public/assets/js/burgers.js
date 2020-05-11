14bca87394713acefd8ea2c7da1eb0d9a9770920
unknown <pak.phil7@gmail.com>
2020-05-11 13:52:56 -0700

    added an undo button and a form to make burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -17,5 +17,45 @@ $(document).ready(function() {
                 location.reload();
             }
         )
-    })
+    });
+    $(".undo").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devoured: false
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                location.reload();
+            }
+        )
+    });
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newBurger = {
+          burger_name: $("#makeBurger").val().trim(),
+          devoured: 0
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("Added new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
 })
