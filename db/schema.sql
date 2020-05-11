064ff4a54732d61a6c0285aacd115fb699c47192
unknown <pak.phil7@gmail.com>
2020-05-10 17:21:00 -0700

    created the orm and added a select, create, and update function

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR (30),
+    devoured BOOLEAN DEFAULT (0),
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Big Mac")
+VALUES ("Whiskey River BBQ")
+VALUES ("Double Double")
